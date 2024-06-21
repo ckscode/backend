372de8988a34c949f0482414b56d335b8e5fb70c
ckscode <cksiddhaarthan@gmail.com>
2024-06-21 18:26:26 +0530

    reset password

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -229,6 +229,7 @@ export const forgotPassword = asyncHandler(async(req,res)=>{
     }
    //create Reset token
    let resetToken = crypto.randomBytes(32).toString("hex") + user._id;
+   console.log(resetToken) 
 
    const hashedToken = crypto.createHash("sha256").update(resetToken).digest("hex");
 
@@ -264,5 +265,29 @@ export const forgotPassword = asyncHandler(async(req,res)=>{
 
 
 export const resetPassword = asyncHandler(async(req,res)=>{
-  res.send('reset password')
+       const {password} = req.body;
+       const {resetToken} = req.params
+
+       //hash the reset token and compare
+  const hashedToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+
+  //find token in the DB
+  const userToken = await Token.findOne({
+    token:hashedToken,
+    expiresAt :{$gt:Date.now()}
+  })
+
+  if(!userToken){
+    res.status(404)
+    throw new Error("Invalid or expired token")
+  }
+
+  //Find user
+  const user = await User.findOne({_id:userToken.userId});
+  user.password = password
+  await user.save()
+  res.status(200).json({
+    message:"Password reset successful"
+  })
+
 })
